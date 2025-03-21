d566a5fb100acbf8a80fd3560b7b1e8048a54cce
Leonardo Marquez <135670067+LeoJzr@users.noreply.github.com>
2025-03-20 19:05:58 -0600

    coords commit

---
+++ service/coords.js
@@ -0,0 +1,9 @@
+module.exports = function getCoords (x, y) {
+  return [
+    { x: x + 530, y: y + 1 },
+    { x: x + 535, y: y + 1 },
+    { x: x + 540, y: y + 1 },
+    { x: x + 545, y: y + 1 },
+    { x: x + 560, y: y + 1 }
+  ]
+}
